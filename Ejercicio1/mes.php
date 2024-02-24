2f232f434eec1783c81c64c528db9ba48b37d580
mariars89 <mec51572@educastur.es>
2024-02-24 10:09:40 +0100

    Ejercicio 1 modificado y acabado

--- Ejercicio1/mes.php
+++ Ejercicio1/mes.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Mes Actual</title>
-</head>
-<body>
-    <h1>Mes Actual</h1>
-    <?php
-        echo "El mes actual es: " . date("F");
-    ?>
-</body>
-</html>
+<?php
+echo "Este es el mes actual: " . date("F");
+?>
